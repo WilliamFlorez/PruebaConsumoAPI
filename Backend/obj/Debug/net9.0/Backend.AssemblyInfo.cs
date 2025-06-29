29648669d157667739bfc199aa3d8783c4196341
WilliamDavid <171434475+WilliamFlorez@users.noreply.github.com>
2025-06-29 18:31:21 -0500

    ultimas actualizaciones

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e86e67d383ae4387a86a15ec647609bf1fe6c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+442f37d04d0268148759bfa23ccb711d7a14f9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
